b3b8cbec649896d5aba716a58cceba4ea8ddb59b
Jplp1919 <jplpetter@gmail.com>
2022-11-09 21:10:30 -0300

    Desenvolvimento da classe que escreve o arquivo xml

--- src/trab02/TestMainOld.java
+++ src/trab02/TestMainOld.java
@@ -3,6 +3,8 @@ package trab02;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
+import trab02.doc.DocumentWriter;
+import trab02.doc.ManipuladorXML;
 
 public class TestMainOld {
 
@@ -14,9 +16,9 @@ public class TestMainOld {
         // dao.saveEscritor(king);
         // Livro it = new Livro("Carrie", "Terror", "978-0-385-08695-0", 25.00, 1 );
         //dao.saveLivro(it);
-        List<Livro> listadeLivros = dao.listarLivros();
+       // List<Livro> listadeLivros = dao.listarLivros();
         //List<Escritor> listadeEscritores = dao.listarEscritores();
-       listadeLivros.stream().forEach(lp -> System.out.println(lp));
+       //listadeLivros.stream().forEach(lp -> System.out.println(lp));
       /*  listadeEscritores.stream().forEach(le -> {
            System.out.println(le.getSobreNome() + ", " + le.getPrimeiroNome());
             try {
@@ -32,6 +34,10 @@ public class TestMainOld {
     //  List<Escritor> listadeEscritores = dao.listarInfo();
      // for (int i =0; i < listadeEscritores.size(); i++){
       //    System.out.println(listadeEscritores.get(i));
+      
+        DocumentWriter dw = new DocumentWriter(con);
+        dw.writeEscritores();
+              
       }
     }
 
--- src/trab02/doc/DocumentWriter.java
+++ src/trab02/doc/DocumentWriter.java
@@ -1,6 +1,68 @@
 package trab02.doc;
 
+import java.util.List;
+import org.w3c.dom.Document;
+import trab02.Escritor;
+import trab02.PersistDAO;
+import java.sql.Connection;
+import java.sql.SQLException;
+import org.w3c.dom.Element;
 
 public class DocumentWriter {
-    
+    //  Document doc;
+
+    Document doc = ManipuladorXML.newDocument();
+    Connection con;
+
+    public DocumentWriter(Connection con) {
+        this.con = con;
+    }
+
+    /**
+     * <Escritor>
+     * <Id> ... </Id>
+     * <Nome> ... </Nome>
+     * <Sobrenome> ... </Sobrenome>
+     * </Escritor>
+     *
+     */
+    public void writeEscritor(Escritor escritor, Element master) {
+        // PersistDAO dao = new  PersistDAO(con);
+
+        //<Escritor>
+        Element root = doc.createElement("Escritor");
+        master.appendChild(root);
+
+        // <Id>
+        Element id = doc.createElement("Id");
+        id.setTextContent(Integer.toString(escritor.getId()));
+        root.appendChild(id);
+
+        // <Nome>
+        Element nome = doc.createElement("Nome");
+        nome.setTextContent(escritor.getPrimeiroNome());
+        root.appendChild(nome);
+        
+       // <Sobrenome>
+       Element sobrenome = doc.createElement("Sobrenome");
+       sobrenome.setTextContent(escritor.getSobreNome());
+       root.appendChild(sobrenome);
+       
+        
+    }
+
+    public void writeEscritores() throws SQLException {
+        PersistDAO dao = new PersistDAO(con);
+        List<Escritor> listaEscritores = dao.listarEscritores();
+        Escritor escritor ;
+        Element master = doc.createElement("Escritores");
+        doc.appendChild(master);
+        for (int i =0; i < listaEscritores.size(); i++){
+         escritor = listaEscritores.get(i);
+         this.writeEscritor(escritor, master);
+         
+      }
+       ManipuladorXML.writeXmlFile(doc, "./escritores.xml");
+    }
+
 }
