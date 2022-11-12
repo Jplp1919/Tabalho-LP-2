7d2df736c5eec8e616bd838596a3d20c73416a0a
Jplp1919 <88355957+Jplp1919@users.noreply.github.com>
2022-11-11 22:04:08 -0300

    metodo para leitura do xml de escritores desenvolvido

--- src/trab02/TestMainOld.java
+++ src/trab02/TestMainOld.java
@@ -1,14 +1,21 @@
 package trab02;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPathExpressionException;
+import org.xml.sax.SAXException;
+import trab02.doc.DocumentReader;
 import trab02.doc.DocumentWriterEscritor;
 import trab02.doc.ManipuladorXML;
 
 public class TestMainOld {
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws SQLException, XPathExpressionException {
 
         // Escritor king = new Escritor("Stephen", "King");   
         Connection con = new ConnectionFactory().establishConnection();
@@ -34,10 +41,18 @@ public class TestMainOld {
     //  List<Escritor> listadeEscritores = dao.listarInfo();
      // for (int i =0; i < listadeEscritores.size(); i++){
       //    System.out.println(listadeEscritores.get(i));
-      
-        DocumentWriterEscritor dw = new DocumentWriterEscritor(con);
-        dw.writeEscritores();
-              
+        
+       // DocumentWriterEscritor dw = new DocumentWriterEscritor(con);
+        //dw.writeEscritores();
+        DocumentReader dr = new DocumentReader();
+        try {
+           List<Escritor> listadeEscritores = dr.readEscritores("./escritores.xml");
+           listadeEscritores.stream().forEach(le -> {
+           System.out.println(le.getSobreNome() + ", " + le.getPrimeiroNome());
+           });
+        } catch (SAXException | ParserConfigurationException | IOException ex) {
+            Logger.getLogger(TestMainOld.class.getName()).log(Level.SEVERE, null, ex);
+        }
       }
     }
 
--- src/trab02/doc/DocumentReader.java
+++ src/trab02/doc/DocumentReader.java
@@ -1,13 +1,68 @@
 package trab02.doc;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathExpressionException;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+import trab02.Escritor;
 
 public class DocumentReader {
 
-    public void readEscritores(String path) {
-     
+    public List<Escritor> readEscritores(String path) throws XPathExpressionException, SAXException, ParserConfigurationException, IOException {
+        File file = new File(path);
+        List<Escritor> escritores = new ArrayList<>();
+       
+        DocumentBuilderFactory dbf
+                = DocumentBuilderFactory.newInstance();
+
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document doc = db.parse(file);
+
+        doc.getDocumentElement().normalize();
+        doc.getDocumentElement().getNodeName();
+        NodeList nodeList
+                = doc.getElementsByTagName("Escritor");
+
+        for (int i = 0; i < nodeList.getLength(); ++i) {
+             Escritor escritor = new Escritor();
+            Node node = nodeList.item(i);
+            node.getNodeName();
+            if (node.getNodeType()
+                    == Node.ELEMENT_NODE) {
+                Element tElement = (Element) node;
+                String idString
+                        = tElement
+                                .getElementsByTagName("Id")
+                                .item(0)
+                                .getTextContent();
+                String nome = tElement
+                        .getElementsByTagName(
+                                "Nome")
+                        .item(0)
+                        .getTextContent();
+                String sobrenome = tElement
+                        .getElementsByTagName(
+                                "Sobrenome")
+                        .item(0)
+                        .getTextContent();
+                int id = Integer.parseInt(idString);
+                escritor.setId(id);
+                escritor.setPrimeiroNome(nome);
+                escritor.setSobreNome(sobrenome);
+               escritores.add(escritor);
+            }
+        }
+        return escritores;
     }
 }
